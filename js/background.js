40763c8dc286896609e890e1adad6790c72d1280
Kim in seong <103021181+joinseong96@users.noreply.github.com>
2024-04-08 09:52:16 +0900

    Add files via upload

---
+++ js/background.js
@@ -0,0 +1,7 @@
+const images = ["bg01.jpg", "bg02.jpg", "bg03.jpg", "bg05.jpg", "bg06.jpg"];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+const bgImageBox = document.querySelector(".img_wp");
+const bgImage = document.createElement("img");
+bgImage.src = `../img/${chosenImage}`;
+
+bgImageBox.appendChild(bgImage);
